4d9f48a4503c1770e4097a36f7c5f988f58e934e
klyupav <pasha.klyuchnikov@gmail.com>
2018-02-10 21:59:03 +0500

    Upgraded for cron autorun

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,6 @@ Route::post('/logout', 'Auth\LoginController@logout');
 Route::get('/', 'ParseitController@index');
 
 Route::post('/parser/save-source', 'ParserController@saveSourcePost');
-Route::get('/parseit/sources', 'ParseitController@getSources');
-Route::get('/parseit/rao_rf_pub', 'ParseitController@rao_rf_pub');
-Route::get('/parseit/rss_rf_pub', 'ParseitController@rss_rf_pub');
-Route::get('/parseit/rss_ts_pub', 'ParseitController@rss_ts_pub');
-Route::get('/parseit/rss_pub_gost_r', 'ParseitController@rss_pub_gost_r');
-Route::get('/parseit/rds_rf_pub', 'ParseitController@rds_rf_pub');
-Route::get('/parseit/rds_ts_pub', 'ParseitController@rds_ts_pub');
-Route::get('/parseit/rds_pub_gost_r', 'ParseitController@rds_pub_gost_r');
 Route::get('/parseit/data', 'ParseitController@getData');
 Route::get('/parseit/categories', 'ParseitController@categories');
 Route::get('/parseit/products', 'ParseitController@products');
@@ -49,15 +41,16 @@ Route::resource('/useragent', 'UserAgentController');
 Route::get('/sources/main', 'ParseitController@sources');
 Route::post('/parseit-off-on', 'ParseitController@parseitOffOn');
 Route::get('/test', 'ParserController@test');
-Route::get('/parseit/all-sources', function(){
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rao_rf_pub');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rss_rf_pub');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rss_ts_pub');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rss_pub_gost_r');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rds_rf_pub');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rds_ts_pub');
-    file_get_contents(env('APP_URL').'/parseit/sources?d=Rds_pub_gost_r');
-});
+
+Route::get('/parseit/sources', 'ParseitController@getSources');
+
+Route::get('/parseit/rao_rf_pub', 'ParseitController@rao_rf_pub');
+Route::get('/parseit/rss_rf_pub', 'ParseitController@rss_rf_pub');
+Route::get('/parseit/rss_ts_pub', 'ParseitController@rss_ts_pub');
+Route::get('/parseit/rss_pub_gost_r', 'ParseitController@rss_pub_gost_r');
+Route::get('/parseit/rds_rf_pub', 'ParseitController@rds_rf_pub');
+Route::get('/parseit/rds_ts_pub', 'ParseitController@rds_ts_pub');
+Route::get('/parseit/rds_pub_gost_r', 'ParseitController@rds_pub_gost_r');
 
 Route::get('/log-to-mail/off', function(){
     $environmentName = 'APP_LOGTOEMAIL';
