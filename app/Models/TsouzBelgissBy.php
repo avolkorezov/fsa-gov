54af4ddf67f86da22059d99d66ff9310a0340dbc
Pavel Klyuchniokov <pasha.klyuchnikov@gmail.com>
2020-03-20 20:59:51 +0500

    added export to file

--- app/Models/TsouzBelgissBy.php
+++ app/Models/TsouzBelgissBy.php
@@ -83,10 +83,10 @@ class TsouzBelgissBy extends Model
     public function toCSVRow()
     {
         $products = [];
-        $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {
-            return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
-        },$this->ProductDetails );
-        $ProductDetails = unserialize($fixed_data);
+//        $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {
+//            return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
+//        },$this->ProductDetails );
+        $ProductDetails = unserialize($this->ProductDetails);
         foreach ($ProductDetails as $product)
         {
             if (isset($product->ProductName))
